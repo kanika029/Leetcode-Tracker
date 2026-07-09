89e7b8378384bb434053be9a739ab095b5e583e5
kanika029 <kanika31072006@gmail.com>
2026-07-09 15:09:21 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 3:09:20 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while(temp != null){
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        Collections.sort(list);
+        temp = head;
+        int i = 0;
+        while(temp != null){
+            temp.val = list.get(i++);
+            temp = temp.next;
+        }
+        return head;
+    }
+}
