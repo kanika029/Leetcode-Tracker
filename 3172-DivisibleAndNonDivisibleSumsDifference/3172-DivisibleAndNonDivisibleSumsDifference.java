00b4396986593cb54a8716ce43797da2cba92223
kanika029 <kanika31072006@gmail.com>
2026-07-09 15:05:33 +0530

    Create 3172-DivisibleAndNonDivisibleSumsDifference

---
+++ 3172-DivisibleAndNonDivisibleSumsDifference/3172-DivisibleAndNonDivisibleSumsDifference.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:05:33 PM
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int dsum=0 , nsum=0;
+        for(int i=1;i<=n;i++){
+            if(i % m==0){
+                dsum+=i;
+            }else{
+                nsum+=i;
+            }
+        }
+        return nsum-dsum;
+    }
+}
