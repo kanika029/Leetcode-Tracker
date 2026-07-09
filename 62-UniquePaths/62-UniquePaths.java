8e06889c0bdc45c8726e77ff3cf9f3723b6bcca3
kanika029 <kanika31072006@gmail.com>
2026-07-09 15:10:11 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 3:10:10 PM
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        int ans = helper(0,0,m,n,dp);
+        return ans;
+    }
+    int helper(int i,int j,int m,int n,int[][]dp){
+        if(i>=m||j>=n) return 0;
+
+        if(i==m-1 && j==n-1) return 1;
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int x = helper(i+1,j,m,n,dp);
+        int y = helper(i,j+1,m,n,dp);
+        dp[i][j] = x+y;
+        return dp[i][j];
+    }
+}
