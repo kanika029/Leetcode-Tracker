cdadbfbcd1fe3ed27ff4ae0cc39e1255b701d906
kanika029 <kanika31072006@gmail.com>
2026-07-09 15:06:31 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 3:06:30 PM
+class Solution {
+    public int minInsertions(String s) {
+                int n = s.length();
+        int[] dp = new int[n];
+        for (int i = n - 2; i >= 0; i--) {
+            int prev = 0;
+            for (int j = i + 1; j < n; j++) {
+                int temp = dp[j];
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j-1]) + 1;
+                }
+                prev = temp;
+            }
+        }
+        return dp[n-1];
+    }
+}
